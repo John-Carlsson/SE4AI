361387d52c05bfbb68776f0f6fb7bf5a854954b0
Sarah Ternus <sarahternus@MacBook-Pro-von-Sarah.local>
2023-05-22 23:42:52 +0200

    extended gui with feedback when classified wrong

--- gui.py
+++ gui.py
@@ -68,6 +68,16 @@ class App:
         self.bad_button.pack(side='bottom')
         self.good_button.pack(side='bottom')
 
+        # emotion label: 'Angry', 'disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'
+        self.happy_button = Button(self.master, text='Happy', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(3))
+        self.sad_button = Button(self.master, text='Sad', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(4))
+        self.neutral_button = Button(self.master, text='Neutral', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(6))
+        self.fear_button = Button(self.master, text='Fear', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(2))
+        self.angry_button = Button(self.master, text='Angry', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(0))
+        self.suprise_button = Button(self.master, text='Surprise', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(5))
+        self.disgust_button = Button(self.master, text='Disgust', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(1))
+
+
         self.text = Text(self.master, height=20, width=100, bg='skyblue')
         self.text.pack(side='top')
         self.face = None
@@ -95,29 +105,63 @@ class App:
             self.feedback = 1
             self.current_frame = None  # Disable stillframe
             self.show_video()
-            self.update_model_with_feedback()
+            self.update_model_with_feedback(1000)
 
     def bad_feedback(self):
         """Handle the 'False' button click event."""
         if self.current_frame is not None:
+            
+            self.happy_button.pack(side='left')
+            self.sad_button.pack(side='left')
+            self.neutral_button.pack(side='left')
+            self.fear_button.pack(side='left')
+            self.angry_button.pack(side='left')
+            self.suprise_button.pack(side='left')
+            self.disgust_button.pack(side='left')
+
+
+    def bad_real_emotion(self, true_emotion):
+        """Handle the 'False' button click event."""
+        if self.current_frame is not None:
+            
+            #how does feedback function work?
+            self.result = true_emotion
+
+            self.happy_button.pack_forget()
+            self.sad_button.pack_forget()
+            self.neutral_button.pack_forget()
+            self.fear_button.pack_forget()
+            self.angry_button.pack_forget()
+            self.suprise_button.pack_forget()
+            self.disgust_button.pack_forget()
+
+
             self.current_frame = None  # Disable stillframe
             self.show_video()
-            self.update_model_with_feedback()
+            self.update_model_with_feedback(true_emotion)
             # Since we dont know what is wrong we can't update with any precise labels
             # this becomes very impractible with more than 2 labels
             # so this is just for possible use in for example a binary classifier
             
-
-    def update_model_with_feedback(self):
+    def update_model_with_feedback(self, true_emotion):
+        print("update model")
         """Update the model with the provided feedback."""
         if self.feedback is not None:
             img_batch = np.expand_dims(self.face, axis=0)
             
-            idx = np.where(max(self.result[0]) == self.result[0])
-            self.result[0] = [0 for i in range(len(self.result[0]))]
-            self.result[0][idx] = 1
+            feedback_data = 0
+            # true_emotion == 1000 represents correct prediction -> correct prediction gets saved
+            if true_emotion == 1000:
+                idx = np.where(max(self.result[0]) == self.result[0])
+                self.result[0] = [0 for i in range(len(self.result[0]))]
+                self.result[0][idx] = 1
 
-            feedback_data = self.result
+                feedback_data = self.result
+                print("feedback data: " + str(feedback_data))
+            # if prediction is wrong and true emotion was given
+            else:
+                feedback_data = true_emotion
+                print("feedback data: " + str(feedback_data))
 
             optimizer = Adam(lr=0.001)
             self.model.compile(loss='binary_crossentropy', optimizer=optimizer)
