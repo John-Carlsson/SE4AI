0257938fafcb01992f0e1e477b80f044efc05365
Sarah Ternus <sarahternus@MacBook-Pro-von-Sarah.local>
2023-05-23 00:37:08 +0200

    retraining with negative feeback

--- gui.py
+++ gui.py
@@ -69,13 +69,13 @@ class App:
         self.good_button.pack(side='bottom')
 
         # emotion label: 'Angry', 'disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'
-        self.happy_button = Button(self.master, text='Happy', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(3))
-        self.sad_button = Button(self.master, text='Sad', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(4))
-        self.neutral_button = Button(self.master, text='Neutral', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(6))
-        self.fear_button = Button(self.master, text='Fear', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(2))
-        self.angry_button = Button(self.master, text='Angry', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(0))
-        self.suprise_button = Button(self.master, text='Surprise', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(5))
-        self.disgust_button = Button(self.master, text='Disgust', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(1))
+        self.happy_button = Button(self.master, text='Happy', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 0, 0, 1, 0, 0, 0]])))
+        self.sad_button = Button(self.master, text='Sad', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 0, 0, 0, 1, 0, 0]])))
+        self.neutral_button = Button(self.master, text='Neutral', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 0, 0, 0, 0, 0, 1]])))
+        self.fear_button = Button(self.master, text='Fear', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 0, 1, 0, 0, 0, 0]])))
+        self.angry_button = Button(self.master, text='Angry', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[1, 0, 0, 0, 0, 0, 0]])))
+        self.suprise_button = Button(self.master, text='Surprise', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 0, 0, 0, 0, 1, 0]])))
+        self.disgust_button = Button(self.master, text='Disgust', font=('arial', 25), fg='black', command=lambda: self.bad_real_emotion(np.array([[0, 1, 0, 0, 0, 0, 0]])))
 
 
         self.text = Text(self.master, height=20, width=100, bg='skyblue')
@@ -105,7 +105,7 @@ class App:
             self.feedback = 1
             self.current_frame = None  # Disable stillframe
             self.show_video()
-            self.update_model_with_feedback(1000)
+            self.update_model_with_feedback(np.array([[0, 0, 0, 0, 0, 0, 0]]))
 
     def bad_feedback(self):
         """Handle the 'False' button click event."""
@@ -146,26 +146,25 @@ class App:
     def update_model_with_feedback(self, true_emotion):
         print("update model")
         """Update the model with the provided feedback."""
-        if self.feedback is not None:
-            img_batch = np.expand_dims(self.face, axis=0)
-            
-            feedback_data = 0
-            # true_emotion == 1000 represents correct prediction -> correct prediction gets saved
-            if true_emotion == 1000:
-                idx = np.where(max(self.result[0]) == self.result[0])
-                self.result[0] = [0 for i in range(len(self.result[0]))]
-                self.result[0][idx] = 1
+        #if self.feedback is not None:
+        img_batch = np.expand_dims(self.face, axis=0)
+        
+        # true_emotion == 0 array represents correct prediction -> correct prediction gets saved
+        if np.array_equal(true_emotion, np.array([[0, 0, 0, 0, 0, 0, 0]])):
+            idx = np.where(max(self.result[0]) == self.result[0])
+            self.result[0] = [0 for i in range(len(self.result[0]))]
+            self.result[0][idx] = 1
 
-                feedback_data = self.result
-                print("feedback data: " + str(feedback_data))
-            # if prediction is wrong and true emotion was given
-            else:
-                feedback_data = true_emotion
-                print("feedback data: " + str(feedback_data))
+            feedback_data = self.result
+            print("feedback data: " + str(feedback_data))
+        # if prediction is wrong and true emotion was given
+        else:
+            feedback_data = true_emotion
+            print("feedback data false: " + str(feedback_data))
 
-            optimizer = Adam(lr=0.001)
-            self.model.compile(loss='binary_crossentropy', optimizer=optimizer)
-            self.model.fit(img_batch, feedback_data, epochs=1)
+        optimizer = Adam(learning_rate=0.001)
+        self.model.compile(loss='binary_crossentropy', optimizer=optimizer)
+        self.model.fit(img_batch, feedback_data, epochs=1)
 
         self.feedback = None
 
