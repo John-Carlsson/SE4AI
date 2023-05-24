e01f4d29aa15b2c72c4467cc1df0d54b458ac5c0
DreckigerDaan <65292718+DreckigerDaan@users.noreply.github.com>
2023-05-24 12:58:27 +0200

    launch() und publish_emotion()

--- SER/gui_audio.py
+++ SER/gui_audio.py
@@ -4,87 +4,133 @@ import time
 import threading
 import tkinter as tk
 import pyaudio
+import np
+
+#from main_ser import add_data_to_queue
+
+
+audio_array = []
+
+
 
 class VoiceRecorder:
 
-    def __init__(self):
+
+
+
+    def launch(self):
         self.window = tk.Tk()
 
-        #generate base window
+        self.window.geometry("1000x650")
+        self.window.title("Emotional Recognition through Speech")
 
-        self.output_console = tk.Text(self.window)
-        self.output_console.pack()
-        self.window.resizable(False, False)
+        self.phonological_var = tk.StringVar()
+        self.linguistic_var = tk.StringVar()
 
-        #add the record button
+
+        self.phonological_label = tk.Label(self.window, text="Phonological Prediction",font=("Robot", 18) )
+        self.phonological_label.pack()
+
+        self.phonological_prediction = tk.Label(self.window, borderwidth=2, relief="groove", textvariable=self.phonological_var,
+                                                width=15, height=3, font=("Robot", 10))
+        self.phonological_prediction.pack()
+
+
+        self.linguistic_label = tk.Label(self.window, text="Linguistic Prediction",  font=("Robot", 18))
+        self.linguistic_label.pack()
+
+        self.linguistic_prediction = tk.Label(self.window,borderwidth=2, relief="groove",  textvariable=self.linguistic_var,
+                                              width=15, height=3, font=("Robot", 10))
+        self.linguistic_prediction.pack()
+
         self.button = tk.Button(text="Record", font=("Robot", 80, "bold"),
-                                command=self.click_handler)
+                                command=self.record_signal)
         self.button.pack()
-        self.label_time = tk.Label(text="00:00")
-        self.label_time.config(font=("Robot", 18, "bold"))
-        self.label_time.pack()
-
-        self.label_phonological = tk.Label(text="Phonological Prediciton")
-        self.label_phonological.config(font=("Robot", 18, "bold"))
-        self.label_phonological.pack()
-
-        self.label_linguistic = tk.Label(text="Linguistic Prediciton")
-        self.label_linguistic.config(font=("Robot", 18, "bold"))
-        self.label_linguistic.pack()
 
 
-
+        self.time_label = tk.Label(text="00:00")
+        self.time_label.pack()
 
         self.recording = False
         self.window.mainloop()
 
-    def click_handler(self):
+
+
+    def record_signal(self):
         if self.recording:
             self.recording = False
             self.button.config(fg="black")
         else:
             self.recording = True
             self.button.config(fg="red")
+            print("Record started")
             threading.Thread(target=self.record).start()
+
+
 
     def record(self):
         audio = pyaudio.PyAudio()
         stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100,
                             input=True, frames_per_buffer=1024)
 
-        frames = []
 
         start = time.time()
 
         while self.recording:
-            data = stream.read(1024)
-            frames.append(data)
+            #read the data from the stream
+            audio_data = stream.read(1024)
+            audio_array.append(np.frombuffer(audio_data, dtype=np.float32))
+
+
 
             passed = time.time() - start
 
             secs = passed % 60
             mins = passed // 60
-            self.label.config(text=f"{int(mins):02d}:{int(secs):02d}")
+            self.time_label.config(text=f"{int(mins):02d}:{int(secs):02d}")
 
+            if passed >= 5:
+                self.recording = False
+                self.button.config(fg="black")
+                #only for testing
+                #self.publish_emotion_label("hallo", "moin")
+
+        print("Record stopped")
         stream.stop_stream()
         stream.close()
         audio.terminate()
 
-        exists = True
-        i = 1
+        audio_array_converted = np.concatenate(audio_array)
 
-        while exists:
-            if os.path.exists(f"recording{i}.wav"):
-                i += 1;
-            else:
-                exists = False
-
-        sound_file = wave.open(f"recording{i}.wav", "wb")
-        sound_file.setnchannels(1)
-        sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-        sound_file.setframerate(44100)
-        sound_file.writeframes(b"".join(frames))
-        sound_file.close()
+        #add_data_to_queue(audio_array_converted)
 
 
-VoiceRecorder()
+
+
+    def publish_emotion_label(self, prediction_1, prediction_2):
+
+        self.phonological_var.set(prediction_1)
+        self.linguistic_var.set(prediction_2)
+
+
+        #exists = True
+        #i = 1
+
+        #while exists:
+        #   if os.path.exists(f"recording{i}.wav"):
+        #        i += 1;
+        #    else:
+        #        exists = False
+
+        #sound_file = wave.open(f"recording{i}.wav", "wb")
+        #sound_file.setnchannels(1)
+        #sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        #sound_file.setframerate(44100)
+        #sound_file.writeframes(b"".join(frames))
+        #sound_file.close()
+
+
+
+if __name__ == "__main__":
+    vc = VoiceRecorder()
+    vc.launch()
