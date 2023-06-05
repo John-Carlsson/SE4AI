508720dd2280e8387cfab9e0f383826194e29809
DreckigerDaan <65292718+DreckigerDaan@users.noreply.github.com>
2023-06-05 21:50:08 +0200

    adding methods to fer_gui

--- gui_combined.py
+++ gui_combined.py
@@ -7,7 +7,15 @@ import platform
 import data_collector as dc
 from functools import partial
 from keras.optimizers import Adam
+import os
+import wave
+import time
+import threading
+import pyaudio
+import pandas as pd
 
+audio_array = []
+recorded_dataframes = []
 
 class Camera:
 
@@ -225,6 +233,87 @@ class App:
 
         # result = self.model(self.face)
         self.text.insert(END, self.text.insert(END, self.to_string() + '\n'))
+
+    def record_signal(self):
+        if self.recording:
+            self.recording = False
+            self.button.config(fg="black")
+        else:
+            self.recording = True
+            self.button.config(fg="red")
+            print("Record started")
+            threading.Thread(target=self.record).start()
+
+    def record(self):
+        audio = pyaudio.PyAudio()
+        stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000,
+                            input=True, frames_per_buffer=512)
+
+        start = time.time()
+
+        while self.recording:
+            # read the data from the stream
+            audio_data = stream.read(1024)
+            audio_array.append(np.frombuffer(audio_data, dtype=np.float32))
+
+            passed = time.time() - start
+
+            secs = passed % 60
+            mins = passed // 60
+            self.time_label.config(text=f"{int(mins):02d}:{int(secs):02d}")
+
+            if passed >= 5:
+                self.recording = False
+                self.button.config(fg="black")
+                # only for testing
+                # self.publish_emotion_label("hallo", "moin")
+
+        print("Record stopped")
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        audio_array_converted = np.concatenate(audio_array)
+        self.set_audio_for_feedback(audio_array_converted)
+
+        from main_ser import add_data_to_queue
+        add_data_to_queue(self, audio_array_converted)
+
+        create_dataframe_audio_and_feedback(self)
+
+        def update_label_text(self, shared_variable):
+            new_text = shared_variable
+            self.phonological_var.set(new_text)
+
+        def set_user_feedback(self, emotion):
+            self.user_feedback_emotion = emotion
+
+        def get_user_feedback(self):
+            return self.user_feedback_emotion
+
+        def set_audio_for_feedback(self, recorded_audio):
+            self.recorded_audio = recorded_audio
+
+        def get_audio_for_feedback(self):
+            return self.recorded_audio
+
+def create_dataframe_audio_and_feedback(self):
+
+        emotion = self.get_user_feedback()
+        audio = self.get_audio_for_feedback()
+
+        data = {'Emotion': [emotion], 'Audio Array': [audio]}
+        df = pd.DataFrame(data)
+        recorded_dataframes.append(df)
+
+        print(recorded_dataframes)
+
+
+def publish_emotion_label(self, prediction_1, prediction_2):
+    global shared_variable
+    shared_variable = prediction_1[0]
+    self.update_label_text(shared_variable)
+
 
 if __name__ == '__main__':
     root = Tk()
